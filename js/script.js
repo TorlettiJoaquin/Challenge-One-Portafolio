46eb3744de04a5d9244871336e8f9ed5cb5d7986
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-03-06 20:24:25 -0300

    Actualizacion menu hb

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ menuIcon.onclick = function () {
         console.log("menu cerrado");
     }
 };
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos / calcHeight) * 100);
+    if (pos > 300) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#b9a0d0 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
